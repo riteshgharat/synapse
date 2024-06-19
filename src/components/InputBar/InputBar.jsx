703f55901c72467bd7b64d8065f607d859a5bbad
Saragorule13 <gorulesaravk13@gmail.com>
2024-06-19 18:54:36 +0530

    Changes in input section

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -8,6 +8,7 @@ function InputBar() {
   return <div className="input w-full h-20 bg-white">
     <div className="search-box flex items-center  h-12 mt-4 mr-28 mb-9 ml-8 rounded-lg justify-between " >
       <input type="textarea" placeholder="Message Tutor" className="bg-transparent text-wrap ml-6 mr-2 w-full flex-1"/>
+    
       <div className="flex items-center gap-3 mr-2 justify-center">
         <img className="h-7 w-7" src={add_images} alt="add image icon" />
         <img className="h-7 w-7" src={camera} alt="camera" />
