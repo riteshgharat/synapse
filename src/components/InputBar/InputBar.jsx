49265a0af4632751617b80530cd08dc500b1c7e5
Saragorule13 <gorulesaravk13@gmail.com>
2024-06-19 17:27:57 +0530

    Changes made in input section

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import './InputBar.css';
+import add_images from '../../assets/add_images.svg'
+import camera from '../../assets/camera.svg'
+import send2 from '../../assets/send2.svg'
 
 function InputBar() {
-  return <div className="w-full h-20 bg-gray-500">InputBar</div>;
+  return <div className="input w-full h-20">
+    <div className="search-box flex" >
+      <input type="text" placeholder="Message Tutor" />
+      <div className="flex">
+        <img src={add_images} alt="add image icon" />
+        <img src={camera} alt="camera" />
+        <img src={send2} alt="send icon" />
+      </div>
+    </div>
+  </div>;
 }
 
 export default InputBar;
--- src/components/LearningSession/LearningSession.jsx
+++ src/components/LearningSession/LearningSession.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./LearningSession.css";
 
 function LearningSession() {
-  return <div className="w-full flex-grow bg-green-500">LearningSession</div>;
+  return (
+    <div className="ls-main w-full flex-grow">
+      <div className="p-tag">
+        <p className="title">
+          Hello, <span>Dev</span>
+        </p>
+        <p className="greet">How can I help you today?</p>
+      </div>
+      <div className="cards flex">
+        <div className="card card-1">
+          <p>Help me solve a algebric problem</p>
+        </div>
+        <div className="card card-2">
+          <p>Help write a short story.</p>
+        </div>
+        <div className="card card-3">
+          <p>Help understand Quantum Mechanics</p>
+        </div>
+        <div className="card card-4">
+          <p>Help understand Vectors</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LearningSession;
