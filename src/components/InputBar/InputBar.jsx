783fb8b1c3c480ad47f07e4fe56ab1df98de2438
Saragorule13 <gorulesaravk13@gmail.com>
2024-06-19 18:35:26 +0530

    Changes in input section

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -5,13 +5,13 @@ import camera from '../../assets/camera.svg'
 import send2 from '../../assets/send2.svg'
 
 function InputBar() {
-  return <div className="input w-full h-20">
-    <div className="search-box flex" >
-      <input type="text" placeholder="Message Tutor" />
-      <div className="flex">
-        <img src={add_images} alt="add image icon" />
-        <img src={camera} alt="camera" />
-        <img src={send2} alt="send icon" />
+  return <div className="input w-full h-20 bg-white">
+    <div className="search-box flex items-center  h-12 mt-4 mr-28 mb-9 ml-8 rounded-lg justify-between " >
+      <input type="textarea" placeholder="Message Tutor" className="bg-transparent text-wrap ml-6 mr-2 w-full flex-1"/>
+      <div className="flex items-center gap-3 mr-2 justify-center">
+        <img className="h-7 w-7" src={add_images} alt="add image icon" />
+        <img className="h-7 w-7" src={camera} alt="camera" />
+        <img className="h-7 w-7" src={send2} alt="send icon" />
       </div>
     </div>
   </div>;
