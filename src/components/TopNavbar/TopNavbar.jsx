1e16de5efabd2cefa784840d0f5783b08da8d27c
Ritesh Gharat <riteshgharat05@gmail.com>
2024-06-21 23:05:43 +0530

    create top navbar

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./TopNavbar.css";
-import NavbarTop from "../../assets/navbar.svg?react";
+import ToggleNavbarSvg from "../../assets/navbar.svg?react";
+
 function TopNavbar() {
-  return <div className="w-full h-12 bg-slate-#3F424A Topnavbar flex flex-row items-center justify-center relative">
-      <h4 className="text-SecondaryText text-center">AI TUTOR</h4>
-      <NavbarTop className=" h-5 w-5 fill-SecondarySvg absolute right-2"/>
-  </div>;
+  return (
+    <div className="w-full h-12 flex flex-row items-center bg-Primary sticky z-0">
+      <ToggleNavbarSvg className="h-6 w-6 md:hidden fill-PrimarySvg"/>
+      <h2 className="w-full text-PrimaryText text-center font-semibold">Synapse AI</h2>
+    </div>
+  );
 }
 
 export default TopNavbar;
