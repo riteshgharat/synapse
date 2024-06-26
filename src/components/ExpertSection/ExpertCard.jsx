110bf9fc3ce485b7aa7cb4f0607b5071ca15ea92
Shettysahil16 <ssahilshetty0516@gmail.com>
2024-06-26 17:56:23 +0530

    make change in VerticalNavbar and ExpertSection

--- src/components/ExpertSection/ExpertCard.jsx
+++ src/components/ExpertSection/ExpertCard.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import "./ExpertCard.css";
 
 export default function ExpertCard() {
   return (
     <div>
-      <div className='h-[21vh] w-[85vw] bg-Quaternary mb-3 md:mb-6 Add-Scrollbar flex flex-row items-center px-6 rounded-2xl md:w-[37vw] md:h-[25vh] cursor-pointer'>
-        
+      <div className='h-[21vh] w-[85vw] bg-Quaternary mb-3 md:mb-6 Add-Scrollbar flex flex-row items-center rounded-2xl md:w-[37vw] md:h-[25vh] cursor-pointer'>
+        <div className='h-full w-[45%] flex flex-row justify-center items-center pl-2 '> 
+          <div className='h-[85%] w-[85%] bg-Primary flex flex-row justify-center items-center rounded-[50%] Card'>
+            faffsefe
+          </div>
+        </div>
+        <div className='h-full w-[55%] flex flex-col justify-center gap-3 items-start md:mt-[-5vh]'>
+          <div className='text-2xl md:text-3xl font-bold'>
+          dgdgdgdg 
+          </div>
+          <div className='text-sm md:text-xl'>
+          dgdgdgdg fsedsf....
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/VerticalNavbar/VerticalNavbar.jsx
+++ src/components/VerticalNavbar/VerticalNavbar.jsx
@@ -22,16 +22,16 @@ function VerticalNavbar() {
           className="h-12 w-[46px] bg-Primary mt-5 py-1 px-1 rounded-[10px] cursor-pointer LogoImage"
           alt="logo"
         />
-        <div className="h-full w-full flex flex-row justify-between my-5 md:flex-col md:mt-20 md:my-0 md:ml-0.5 Hidden">
-        <Learning className="h-10 w-10  md:ml-0 fill-SecondarySvg rounded-[10px] bg-Quaternary md:mb-[-5vh] px-1.5 py-1.5  Hidden" />
-          <Tuition className="h-10 w-10  md:ml-0 fill-SecondarySvg rounded-[10px] bg-Quaternary md:mb-[-5vh] px-2 py-2 Hidden" />
-          <Test className="h-10 w-10  md:ml-0 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 Hidden" />
-          <User className="h-10 w-10  md:ml-0 md:mt-[34vh] fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:hidden Hidden" />
+        <div className="h-[41vh] flex flex-row justify-start gap-4 my-5 md:flex-col md:mt-[13vh] md:my-0 md:ml-[0.2vw] Hidden">
+        <Learning className="h-10 w-10 fill-SecondarySvg rounded-[10px] bg-Quaternary  px-1.5 py-1.5  Hidden" />
+          <Tuition className="h-10 w-10 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 Hidden" />
+          <Test className="h-10 w-10 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 Hidden" />
+          <User className="h-10 w-10 md:mt-[34vh] fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:hidden Hidden" />
         </div>
       </div>
-      <div className="md:mb-[18vh] ml-4 md:ml-0">
-      <User className="h-10 w-10 ml-[11vw] md:ml-0 md:mt-[34vh] fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:mb-4 User" />
-        <Logout className="h-10 w-10 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:mb-2 Logout" />
+      <div className=" h-[25vh] md:mb-[18vh] ml-4 md:ml-0 flex flex-col justify-center mt-2">
+      <User className="h-10 w-10 ml-[11vw] md:ml-0 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:mb-4 User" />
+        <Logout className="h-10 w-10 fill-SecondarySvg rounded-[10px] bg-Quaternary px-2 py-2 md:mb-4 Logout" />
       </div>
     </div>
   );
