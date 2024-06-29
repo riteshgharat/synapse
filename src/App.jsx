21832f33aca4c924d96b7e17681c55d0b7395336
Ritesh Gharat <riteshgharat05@gmail.com>
2024-06-29 16:32:48 +0530

    firebase credentials template

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,11 @@ import SignUp from "./components/SignUp/SignUp";
 import TestMode from "./components/TestMode/TestMode";
 import ExpertSection from "./components/ExpertSection/ExpertSection";
 
+import "./firebase/config";
+
 function App() {
   // useState to manage the visibility of the SubNavbar
-  const [isSubNavVisible, setIsSubNavVisible] = useState(true);
+  const [isSubNavVisible, setIsSubNavVisible] = useState(false);
 
   return (
     <>
@@ -21,12 +23,13 @@ function App() {
         isSubNavVisible={isSubNavVisible}
         setIsSubNavVisible={setIsSubNavVisible}
       />
-      <div className="h-full w-full md:w-[calc(100%-60px-250px)] flex flex-grow flex-col justify-center items-center">
+      <div className="h-full w-full md:w-[calc(100%-60px-250px)] flex flex-grow flex-col justify-center items-center bg-Primary">
         <TopNavbar
           isSubNavVisible={isSubNavVisible}
           setIsSubNavVisible={setIsSubNavVisible}
         />
-       <ExpertSection/>
+        <ChatWindow />
+        <InputBar />
       </div>
     </>
   );
