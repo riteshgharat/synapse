cd2fc79f4646b5b97fe1cb16beb68da875008dba
Shettysahil16 <ssahilshetty0516@gmail.com>
2024-07-02 20:18:16 +0530

    make change in ExpertCard size

--- src/components/ExpertSection/ExpertCard.jsx
+++ src/components/ExpertSection/ExpertCard.jsx
@@ -16,17 +16,17 @@ export default function ExpertCard(props) {
   };
   return (
     <div>
-      <div className="h-[21vh] w-[74vw] md:h-[25vh] md:w-[40vw] lg:h-[25vh] lg:w-[35vw] xl:h-[24vh] xl:w-[31vw]
-       bg-Secondary leading-5 md:leading-[3vh] border-4 hover:border-4 hover:ease-out hover:border-Quaternary hover:delay-75
-       mb-3 md:mb-6 Add-Scrollbar flex flex-row items-center rounded-2xl cursor-pointer CardDesign">
+      <div className="h-[17.5vh] w-[88vw] md:h-[19vh] md:w-[75vw] lg:h-[18vh] lg:w-[40vw] xl:h-[24vh] xl:w-[31vw]
+       bg-Secondary leading-5 md:leading-[2vh] border-4 hover:border-4 hover:ease-out hover:border-Quaternary hover:delay-75
+       mb-3 md:mb-6 Add-Scrollbar flex flex-row items-center rounded-2xl md:rounded-[35px] cursor-pointer CardDesign">
         <div className="h-full w-[40%] flex flex-row justify-center items-center">
           <div className="h-[12vh] w-[90px] md:h-[14vh] md:w-[130px] md:ml[8vw] flex flex-row justify-center items-center rounded-[50%] border-2 md:border-4 border-Quaternary Card"></div>
         </div>
         <div className="w-[55%] text-Quaternary flex flex-col justify-center gap-3 items-start md:px-[1vw] pl-[2vw] md:pl-[1vw] pt-[2vh] md:mb-[3.5vh]">
-          <div className="text-[3vh] md:text-[2.5vh] font-bold w-full">
+          <div className="text-[3vh] md:text-[2.5vh] lg:text-[2vh] xl:text-[2.5vh] leading-6 md:leading-0 font-bold w-full">
             {capitalize(props.title)}
           </div>
-          <div className="text-[2vh] md:text-[2vh] w-full text-xs break-words md:leading-[1.75vh]">
+          <div className="text-[2vh] md:text-[2vh] lg:text-[1.75vh] xl:text-[2vh] w-full text-xs break-words md:leading-[1.75vh]">
             {limitedWords(props.desc)}...
           </div>
         </div>
